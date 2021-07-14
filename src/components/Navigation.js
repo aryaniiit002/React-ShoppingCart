331ae6c43d5eeea97cc81807e40f906752ca26b5
Aryaniiit002 <aryanbindal2015@gmail.com>
2021-07-14 16:25:49 +0530

    commit #1

---
+++ src/components/Navigation.js
@@ -0,0 +1,12 @@
+import React from 'react'
+// import { Link } from 'react-router-dom';
+
+export const Navigation = () => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+export default Navigation;
